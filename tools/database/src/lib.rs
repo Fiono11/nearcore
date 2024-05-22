f274531309f0c24ef9d5a4ef30028dbde55590e8
Fiono11 <ruipedromorais11@gmail.com>
2024-05-22 23:56:59 +0100

    Fix dependencies

---
+++ tools/database/src/lib.rs
@@ -0,0 +1,13 @@
+mod adjust_database;
+mod analyse_data_size_distribution;
+mod analyse_gas_usage;
+mod block_iterators;
+pub mod commands;
+mod compact;
+mod corrupt;
+mod make_snapshot;
+mod memtrie;
+mod run_migrations;
+mod state_perf;
+mod utils;
+mod write_to_db;
